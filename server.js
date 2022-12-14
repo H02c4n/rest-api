acd5edc8963a6fe5f92f51fb062de43b4ebcd250
H02c4n <svtr.hozcan@gmail.com>
2022-12-14 17:48:41 +0100

    auth

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const app = express();
 
+const { MongoClient } = require("mongodb");
+const mongoose = require("mongoose");
+
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
@@ -10,15 +14,34 @@ app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
 
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", true);
+// Replace the following with values for your environment.
+const username = encodeURIComponent("myapi42");
+const password = encodeURIComponent("U41Fn2W431WWZNS0");
+const clusterUrl = "myapi42.5cd1oiy.mongodb.net/";
+const authMechanism = "SCRAM-SHA-1";
 
-mongoose.connect("mongodb://localhost/myapi", { useNewUrlParser: true });
+// Connection URI
+const uri = `mongodb+srv://${username}:${password}@${clusterUrl}/?authMechanism=${authMechanism}`;
 
-const db = mongoose.connection;
-db.on("error", (error) => console.log(error));
-db.once("open", () => console.log("Connected to Mongoose"));
+// Create a new MongoClient
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+
+    // Establish and verify connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to server");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000);
---
+++ server2.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const app = express();
+
+const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+
+mongoose.connect("mongodb://localhost/myapi", { useNewUrlParser: true });
+
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Connected to Mongoose"));
+
+app.use("/", indexRouter);
+app.use("/authors", authorRouter);
+
+app.listen(process.env.PORT || 3000);

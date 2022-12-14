affec133d42b0d0c7985fb906de6838e01f79a1f
H02c4n <svtr.hozcan@gmail.com>
2022-12-14 17:51:48 +0100

    input

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-//const Author = require("../models/author");
+const Author = require("../models/author");
 
 //All Authors
 router.get("/", (req, res) => {
@@ -9,7 +9,7 @@ router.get("/", (req, res) => {
 
 // New Author
 router.get("/new", (req, res) => {
-  res.render("authors/new", { author: "deneme" });
+  res.render("authors/new", { author: new Author() });
 });
 
 //Create author
